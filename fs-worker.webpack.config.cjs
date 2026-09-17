f8a45d48f43cfce8d5b34e581fffbac55222b594
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2026-09-17 15:07:20 +0000

    hurr

---
+++ fs-worker.webpack.config.cjs
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+/**    ---------------------------------------------------------
+ *     fs-worker.webpack.config.cjs
+ *     ---------------------------------------------------------
+ *     Copyright ©
+ *       Pellegrino Prevete
+ *         2025, 2026
+ * 
+ *     All rights reserved
+ *     ---------------------------------------------------------
+ * 
+ *     This program is free software: you can redistribute it and/or
+ *     modify it under the terms of the GNU General Public License
+ *     as published by the Free Software Foundation, either
+ *     version 3 of the License, or (at your option) any later
+ *     version.
+ * 
+ *     This program is distributed in the hope that it will be
+ *     useful, but WITHOUT ANY WARRANTY; without even the implied
+ *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *     PURPOSE.
+ *     See the GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the
+ *     GNU General Public License along with this program.
+ *     If not, see <https://www.gnu.org/licenses/>.
+ */
+
+const
+  _path =
+    require(
+      'path');
+const
+  _output_dir =
+    _path.resolve(
+      __dirname);
+const
+  _input_file_name =
+    "fs-worker";
+const
+  _input_file_path =
+    `./node_modules/opfs/${_input_file_name}`;
+const
+  _output_file_name =
+    `${_input_file_name}.js`;
+const
+  _output = {
+    path:
+      _output_dir,
+    filename:
+      _output_file_name
+  };
+module.exports = {
+  entry:
+    _input_file_path,
+  output:
+    _output,
+  optimization: {
+    moduleIds: 'deterministic',
+  },
+  resolve: {
+    alias: {
+      "fs":
+        _path.resolve(
+          __dirname,
+          "node_modules/fs/fs"),
+      "node:fs":
+        _path.resolve(
+          __dirname,
+          "node_modules/opfs/opfs"),
+      "opfs":
+        _path.resolve(
+          __dirname,
+          'node_modules/opfs/opfs'),
+      "path":
+        _path.resolve(
+          __dirname,
+          'node_modules/path/mod.js'),
+    },
+  }
+};
